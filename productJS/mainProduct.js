e83fe8e4a9b4935a85b09ac30024e172795556eb
Ignacio Marin <ignaciomaringarro@gmail.com>
2023-05-30 13:47:09 -0600

    advance of part2

---
+++ productJS/mainProduct.js
@@ -0,0 +1,10 @@
+import { initVisualizer } from '../productJS/modulesProduct/visualizer.js';
+import { initDetails } from '../productJS/modulesProduct/details.js';
+import { initProducts } from '../productJS/modulesProduct/otherProducts.js';
+import { initColors } from '../productJS/modulesProduct/color.js';
+
+initVisualizer();
+initDetails();
+initProducts();
+initColors();
+
---
+++ productJS/modulesProduct/visualizer.js
@@ -0,0 +1,30 @@
+import { productChange } from './otherProducts.js';
+import { colorChange } from './color.js';
+
+const image = document.querySelector('#main-img');
+const joke = document.querySelector('#main-joke');
+let state = {
+  color: 'white',
+  product: 'shirt'
+}; 
+
+function handleProductChange(product) {
+  image.setAttribute('src', `images/product-${product}-${state.color}.jpg`);
+  state.product = product;
+}
+
+function handleColorChange(color) {
+  joke.classList.remove(`with-${state.color}-img`);
+  image.setAttribute('src', `images/product-${state.product}-${color}.jpg`);
+  joke.classList.add(`with-${color}-img`);
+  state.color = color;
+}
+
+function initVisualizer() {
+  productChange.subscribe(handleProductChange);
+  colorChange.subscribe(handleColorChange);
+}
+
+export {
+  initVisualizer
+};
