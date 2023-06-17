75fe01d90799fea9d8d7320dbee1b2c61fd70d62
Ignacio Marin <ignaciomaringarro@gmail.com>
2023-06-16 22:34:48 -0600

    homework advance 2

--- main.js
+++ main.js
@@ -71,13 +71,31 @@ async function showEventsByCategory(category) {
   const events = await getEventsByCategory(category);
 
   if (events) {
-    // Si los eventos están en el caché, mostrarlos directamente
+        // Si los eventos están en el caché, mostrarlos directamente
     renderEvents(events);
   } else {
-    // Si los eventos no están en el caché, realizar la solicitud al API y mostrarlos
+        // Si los eventos no están en el caché, realizar la solicitud al API y mostrarlos
     const fetchedEvents = await fetchEventsByCategory(category);
     renderEvents(fetchedEvents);
   }
+
+  const favoriteButtons = document.querySelectorAll('.favorite-btn');
+  const interestedButtons = document.querySelectorAll('.interested-btn');
+  const goingButtons = document.querySelectorAll('.going-btn');
+
+  favoriteButtons.forEach(button => {
+    button.addEventListener('click', toggleFavorite);
+  });
+
+  interestedButtons.forEach(button => {
+    button.addEventListener('click', toggleInterested);
+  });
+
+  goingButtons.forEach(button => {
+    button.addEventListener('click', toggleGoing);
+  });
+
+  // Resto del código para marcar la tab seleccionada y actualizar la interfaz
 }
 
 // Función para renderizar los eventos en la interfaz
@@ -94,11 +112,51 @@ function renderEvents(events) {
       <p>Date: ${formatDate(event.date)}</p>
       <p>Location: ${formatLocation(event.location)}</p>
       <p>Price: ${formatPrice(event.price)}</p>
+      <button class="favorite-btn" data-event-id="${event.id}">
+        <i class="far fa-heart"></i>
+      </button>
+      <button class="interested-btn" data-event-id="${event.id}">
+        Interested
+      </button>
+      <button class="going-btn" data-event-id="${event.id}">
+        Going!
+      </button>
     `;
 
     eventElement.innerHTML = eventHTML;
     eventList.appendChild(eventElement);
   });
+}
+
+function toggleFavorite() {
+  const eventId = this.getAttribute('data-event-id');
+  // Lógica para agregar o remover el evento de la lista de favoritos
+  // Cambiar el estado del ícono de corazón lleno/vacío
+  const icon = this.querySelector('i');
+  icon.classList.toggle('fas');
+  icon.classList.toggle('far');
+}
+
+function toggleInterested() {
+  const eventId = this.getAttribute('data-event-id');
+  // Lógica para agregar o remover el evento de la lista "Interested"
+  if (this.textContent === 'Interested') {
+      // Cambiar el texto del botón a "Remove" o restaurar el texto original
+    this.textContent = 'No longer interested?';
+  } else {
+    this.textContent = 'Interested';
+  }
+}
+
+function toggleGoing() {
+  const eventId = this.getAttribute('data-event-id');
+  // Lógica para agregar o remover el evento de la lista "Going"
+  if (this.textContent === 'Going!') {
+      // Cambiar el texto del botón a "Your going to this event" o restaurar el texto original
+    this.textContent = 'Your going to this event';
+  } else {
+    this.textContent = 'Going!';
+  }
 }
 
 // Función para formatear la fecha en el formato deseado
@@ -116,7 +174,6 @@ function formatLocation(location) {
     return 'Location information not available';
   }
 }
-
 
 // Función para formatear el precio en el formato deseado
 function formatPrice(price) {
